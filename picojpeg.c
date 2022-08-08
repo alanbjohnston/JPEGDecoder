0a346216d17067404ffbe22009a1f0a5177a2477
alanbjohnston <alan.b.johnston@gmail.com>
2022-08-08 04:36:00 -0400

    Update picojpeg.c

--- picojpeg.c
+++ picojpeg.c
@@ -5,6 +5,7 @@
 // Also integrated and tested changes from Chris Phoenix <cphoenix@gmail.com>.
 //------------------------------------------------------------------------------
 #include "picojpeg.h"
+#include <Arduino.h>
 //------------------------------------------------------------------------------
 // Set to 1 if right shifts on signed ints are always unsigned (logical) shifts
 // When 1, arithmetic right shifts will be emulated by using a logical shift
